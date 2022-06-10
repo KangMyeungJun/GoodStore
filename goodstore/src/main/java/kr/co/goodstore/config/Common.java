7e860bbaba0158f5e30886cbaf1a8f531d66e41a
KangMyeungJun <zkzkfkcl23@naver.com>
2022-06-10 23:26:35 +0900

    Common.java commit

--- goodstore/src/main/java/kr/co/goodstore/config/Common.java
+++ goodstore/src/main/java/kr/co/goodstore/config/Common.java
@@ -5,5 +5,8 @@ public final class Common {
 
     public static final String DOMAIN="localhost";
     public static final String STATICPATH="http://localhost/goodstore/resources/";
+    public static final String UPLOADPATH="/goodstore/upload/";
+    public static final String FILE_DIR="C:/Users/user/git/GoodStore/goodstore/src/main/webapp/upload/";
+    public static final String MIDDLEPATH = "/goodstore";
 
 }
